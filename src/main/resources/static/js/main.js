30af5168f358c33fee93968924b0a6b40a94defe
Rafael Moreira <rafaelgumoreira@hotmail.com>
2024-01-29 18:50:19 -0300

    atualizacao codigo

---
+++ src/main/resources/static/js/main.js
@@ -0,0 +1,12 @@
+// function authenticate() {
+//     var username = document.getElementById('username').value;
+//     var password = document.getElementById('password').value;
+
+//     // Simulação: Verificar as credenciais no backend
+//     if (username === 'usuario' && password === 'senha') {
+//         // Redirecionar para a página após o login bem-sucedido
+//         window.location.href = '/paginainicial';
+//     } else {
+//         alert('Credenciais inválidas. Tente novamente.');
+//     }
+// }
