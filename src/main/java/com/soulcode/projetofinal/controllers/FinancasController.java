dacc0e08eaba1a0f61b618f3ed1853005753a8b6
Rafael Moreira <rafaelgumoreira@hotmail.com>
2024-02-01 14:25:20 -0300

    atualizacao quinta

--- src/main/java/com/soulcode/projetofinal/controllers/FinancasController.java
+++ src/main/java/com/soulcode/projetofinal/controllers/FinancasController.java
@@ -2,9 +2,14 @@ package com.soulcode.projetofinal.controllers;
 
 
 import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 import com.soulcode.projetofinal.models.Financeiro;
 import com.soulcode.projetofinal.repositories.FinanceiroRepository;
@@ -24,4 +29,22 @@ public class FinancasController {
         mv.addObject("listaFinancas", financas); //Conecta com o HTML
         return mv; // Objeto configurado com a view e os dados que ela vai usar
     }
+
+     @PostMapping("/financas/delete") // action, method, name
+    public String deleteFinancas(@RequestParam Integer id) { // GET-POST-REDIRECT
+        // @RequestParam no POST = vai procurar o valor com o nome determinado
+        try {
+            FinanceiroRepository.deleteById(id);
+        } catch (Exception e) {
+            // Em caso de algum problema mostra a página de erro
+            return "erro";
+        }
+
+        // Redireciona o usuário para a lista de finanças
+        // após a remoção feita com sucesso
+        return "redirect:/financas";
+    }
+
+
+    
 }
