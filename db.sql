b02e278ec8d7b2ee5b89afef2da566634acaa84f
Rafael Moreira <rafaelgumoreira@hotmail.com>
2024-02-01 01:39:58 -0300

    nova atualização

---
+++ db.sql
@@ -0,0 +1,7 @@
+USE projetofinal_db;
+
+DESCRIBE financeiro;
+
+INSERT INTO financeiro (descricao, valor, tipo, data) VALUES ('Descrição da transação', 100.0, 'entrada', '2022-02-15');
+
+SELECT*FROM financeiro;
--- src/main/java/com/soulcode/projetofinal/controllers/FinancasController.java
+++ src/main/java/com/soulcode/projetofinal/controllers/FinancasController.java
@@ -1,12 +1,27 @@
 package com.soulcode.projetofinal.controllers;
 
+
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+import com.soulcode.projetofinal.models.Financeiro;
+import com.soulcode.projetofinal.repositories.FinanceiroRepository;
 
 @Controller
 public class FinancasController {
+
+    @Autowired
+    private FinanceiroRepository FinanceiroRepository;
+
     @GetMapping("/financas")
-    public String paginaFinancas(){
-        return "financas";
+    public ModelAndView paginaFinancas(){
+        List<Financeiro> financas = FinanceiroRepository.findAll(); //Equivale a SELECT*FROM FINANCAS
+        // Usamos ModelAndView quando precisamos fornecer dados para o HTML
+        ModelAndView mv = new ModelAndView("financas"); //// Indica qual o template/view
+        // Nome da variável do template e o valor dessa variável
+        mv.addObject("listaFinancas", financas); //Conecta com o HTML
+        return mv; // Objeto configurado com a view e os dados que ela vai usar
     }
 }
