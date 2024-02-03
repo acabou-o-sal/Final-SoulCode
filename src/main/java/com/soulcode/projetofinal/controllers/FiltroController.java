08cdb5007b64bdd1eb801f284e0df2e0cf7dd7ef
Rafael Moreira <rafaelgumoreira@hotmail.com>
2024-02-03 15:26:29 -0300

    mudança no resopitory

--- src/main/java/com/soulcode/projetofinal/controllers/FiltroController.java
+++ src/main/java/com/soulcode/projetofinal/controllers/FiltroController.java
@@ -8,21 +8,23 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.soulcode.projetofinal.models.Financeiro;
+import com.soulcode.projetofinal.repositories.FiltroRepository;
 import com.soulcode.projetofinal.repositories.FinanceiroRepository;
 
 @Controller
 public class FiltroController {
    
     @Autowired
-    private FinanceiroRepository FinanceiroRepository;
+    private FiltroRepository FiltroRepository;
 
      @GetMapping("/filtro")
     public ModelAndView paginaFinancas(){
-        List<Financeiro> financas = FinanceiroRepository.findAll(); //Equivale a SELECT*FROM FINANCAS
+        List<Financeiro> financas = FiltroRepository.findAll(); //Equivale a SELECT*FROM FINANCAS
         // Usamos ModelAndView quando precisamos fornecer dados para o HTML
         ModelAndView mv = new ModelAndView("filtro"); //// Indica qual o template/view
         // Nome da variável do template e o valor dessa variável
         mv.addObject("listaFinancas", financas); //Conecta com o HTML
         return mv; // Objeto configurado com a view e os dados que ela vai usar
     }
+    
 }
--- src/main/java/com/soulcode/projetofinal/repositories/FiltroRepository.java
+++ src/main/java/com/soulcode/projetofinal/repositories/FiltroRepository.java
@@ -1,5 +1,8 @@
 package com.soulcode.projetofinal.repositories;
 
+import java.time.LocalDate;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import com.soulcode.projetofinal.models.Financeiro;
